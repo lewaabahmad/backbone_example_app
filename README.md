7be8a1063541e78cb4abed4b6017063520816167
lewaabahmad <lewaabahmad@gmail.com>
2015-05-26 13:19:18 -0400

    done

---
+++ README.md
@@ -0,0 +1,1 @@
+# Backbone Example App
---
+++ js/views/dog-view.js
@@ -0,0 +1,15 @@
+DogView = Backbone.View.extend({
+  el: '#dog', 
+  events: {
+    'click #set': 'setName'
+  }, 
+  setName : function(e) {
+    e.preventDefault;
+    console.log("hi")
+    var name = $('#name').val();
+    this.model.set({name: name}); 
+  },
+  render: function() {
+    this.$el.html(this.model.get('name'));
+  }
+});
